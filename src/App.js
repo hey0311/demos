d1e5807910de2cea1900410ec598585985d72c06
heyongg <heyongg@yonyou.com>
2018-06-23 23:10:49 +0800

    demo03:动画效果

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Demo01 from './demo/Demo01.js'
 import Demo02 from './demo/Demo02.js'
+import Demo03 from './demo/Demo03.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height:'100%'}}>
-          <Demo02/>
+          <Demo03/>
       </div>
     );
   }
---
+++ src/demo/Demo03.js
@@ -0,0 +1,116 @@
+import React, {Component} from 'react';
+import * as d3 from 'd3';
+import './Demo03.css'
+
+//动画,过渡
+class Demo03 extends Component {
+    componentDidMount() {
+        let width = 600, height = 600;
+        //画布周边的空白
+        var padding = {left: 30, right: 30, top: 20, bottom: 20};
+        let svg = d3.select("#demo01")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);//创建svg元素
+        var dataset = [10, 20, 30, 40, 33, 24, 12, 5];
+        //x轴的比例尺,序数
+        var xScale = d3.scale.ordinal()
+            .domain(d3.range(dataset.length))//输入范围
+            .rangeRoundBands([0, width - padding.left - padding.right]);//输出范围,需要去除两边空白
+        //y轴的比例尺
+        var yScale = d3.scale.linear()
+            .domain([0, d3.max(dataset)])
+            .range([height - padding.top - padding.bottom, 0]);
+        //定义x轴
+        var xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom");
+        //定义y轴
+        var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left");
+        //矩形之间的空白
+        var rectPadding = 4;
+        //添加矩形元素
+        var rects = svg.selectAll(".MyRect")
+            .data(dataset)
+            .enter()
+            .append("rect")
+            .attr("class", "MyRect")
+            .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+            .attr("x", function (d, i) {
+                return xScale(i) + rectPadding / 2;
+            })
+            .attr("y", function (d) {
+                var min = yScale.domain()[0];
+                console.log(yScale(min))
+                return yScale(min)+padding.bottom;
+            })
+            .style('fill','yellow')
+            .transition()
+            .delay(function (d, i) {
+                return i * 200;
+            })
+            .duration(1500)
+            .style('fill','steelblue')
+            .attr("y", function (d) {
+                return yScale(d);
+            })
+            .attr("width", xScale.rangeBand() - rectPadding)
+            .attr("height", function (d) {
+                return height - padding.top - padding.bottom - yScale(d);
+            })
+        //添加文字元素
+        var texts = svg.selectAll(".MyText")
+            .data(dataset)
+            .enter()
+            .append("text")
+            .attr("class", "MyText")
+            .attr("transform", "translate(" + padding.left + "," + padding.top + ")")//平移
+            .attr("x", function (d, i) {
+                return xScale(i) + rectPadding / 2;
+            })
+            .attr('y', (d) => {
+                var min = yScale.domain()[0];
+                return yScale(min)
+            })
+            .transition()
+            .duration(1500)
+            .delay((d, i) => {
+                return i * 200;
+            })
+            .attr("y", function (d) {
+                return yScale(d);
+            })
+            .attr("dx", function () {
+                return (xScale.rangeBand() - rectPadding) / 2;
+            })
+            .attr("dy", function (d) {
+                return 20;
+            })
+            .text(function (d) {
+                return d;
+            });
+        //添加x轴
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding.left + "," + (height - padding.bottom) + ")")
+            .call(xAxis);
+        //添加y轴
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+            .call(yAxis);
+    }
+
+    render = () => {
+        return (
+            <div id="demo01" className="d3-container">
+                <p>柱形图</p>
+            </div>
+        );
+    }
+}
+
+export default Demo03;
+
